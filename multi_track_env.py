5837534551f05e6e1bb8b2eba48316a8972d4aeb
mochan <mochan@mit.edu>
2025-12-03 03:08:42 -0500

    push

--- multi_track_env.py
+++ multi_track_env.py
@@ -1,6 +1,7 @@
 import gym
 import numpy as np
 import random
+from utils import get_initial_pose
 
 class MultiTrackEnv(gym.Env):
     def __init__(self, track_paths, make_env_fn):
@@ -16,12 +17,13 @@ class MultiTrackEnv(gym.Env):
         track = random.choice(self.track_paths)
         # print(f"Loading track: {track}")
         self.env = self.make_env_fn(track)
+        self.track_initial_pose = get_initial_pose(track)
         self.action_space = self.env.action_space
         self.observation_space = self.env.observation_space
 
     def reset(self, **kwargs):
         self._load_new_track()
-        return self.env.reset(**kwargs)
+        return self.env.reset(poses=self.track_initial_pose, **kwargs)
 
     def step(self, action):
         obs, reward, done, info = self.env.step(action)
--- multi_track_train.py
+++ multi_track_train.py
@@ -3,27 +3,29 @@ import yaml
 import numpy as np
 import pandas as pd
 import gym
-from argparse import Namespace
+from argparse import ArgumentParser
 from stable_baselines3 import PPO
 from f110_gym.envs.base_classes import Integrator
 from f1tenth_wrapper import F110SB3Wrapper
 from multi_track_env import MultiTrackEnv
 import warnings
 from utils import load_track_config, get_initial_pose, make_env_from_track, get_tracks_list
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import DummyVecEnv
 
 # -----------------------------
 # Config
 # -----------------------------
 TRACKS_DIR = "./envs/f1tenth_racetracks"
 TIMESTEP = 0.01
-TIMESTEPS_PER_TRACK = 1_000_000
+TIMESTEPS_PER_TRACK = 4_000_000
 TRAIN_SPLIT = 0.8
-
+TENSORBOARD_LOG = "./tensorboard/f110_multi_track_ppo/"
 
 # -----------------------------
 # Main training function
 # -----------------------------
-def main():
+def main(model_path=None):
     # Detect tracks
     track_paths = get_tracks_list(TRACKS_DIR)
     if not track_paths:
@@ -32,8 +34,8 @@ def main():
     print(f"Found {len(track_paths)} tracks.")
 
     # Split into training/validation sets
-    # num_train = int(len(track_paths) * TRAIN_SPLIT)
-    num_train = 1
+    num_train = max(1, int(len(track_paths) * TRAIN_SPLIT))
+    # num_train = 5
     train_tracks = track_paths[:num_train]
     val_tracks = track_paths[num_train:]
 
@@ -48,31 +50,51 @@ def main():
     # -----------------------------
     # Multi-track training environment
     # -----------------------------
-    train_env = MultiTrackEnv(train_tracks, lambda p: make_env_from_track(p, use_raceline=True))
+    initial_pose = get_initial_pose(train_tracks[0])
+    # train_env = MultiTrackEnv(train_tracks, lambda p: make_env_from_track(p, use_raceline=True))
+    x = lambda : MultiTrackEnv(train_tracks, lambda p: make_env_from_track(p, use_raceline=True))
+    train_env = make_vec_env(
+        x,
+        n_envs=4,
+        vec_env_cls=DummyVecEnv,
+    )
     train_env.reset()
-
     # -----------------------------
     # Stable-Baselines3 PPO agent
     # -----------------------------
-    model = PPO(
-        "MultiInputPolicy",
-        train_env,
-        verbose=1,
-        tensorboard_log="./tensorboard/f110_multi_track_ppo/",
-        learning_rate=3e-4,
-        n_steps=2048,
-        batch_size=512,
-        n_epochs=10,
-        gamma=0.99,
-        gae_lambda=0.95,
-        clip_range=0.2,
-        ent_coef=0.0,
-    )
+    if model_path and os.path.exists(model_path):
+        print(f"Loading pre-trained model from {model_path}")
+        model = PPO.load(model_path, env=train_env)
+    else:
+        print("Training from scratch")
+        model = PPO(
+            "MultiInputPolicy",
+            train_env,
+            verbose=1,
+            tensorboard_log=TENSORBOARD_LOG,
+            learning_rate=3e-4,
+            n_steps=2048,
+            batch_size=512,
+            n_epochs=10,
+            gamma=0.99,
+            gae_lambda=0.95,
+            clip_range=0.2,
+            ent_coef=0.0,
+        )
 
-    # Train
+    # -----------------------------
+    # Train / Fine-tune
+    # -----------------------------
     model.learn(total_timesteps=TIMESTEPS_PER_TRACK * len(train_tracks))
-    model.save("results/ppo_f110_multi_track_use_scan")
-    print("Training complete and saved!")
+
+    # -----------------------------
+    # Save
+    # -----------------------------
+    name = "ppo_f110_all_train"
+    save_path = f"results/{name}_finetuned" if model_path else f"results/{name}"
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    model.save(save_path)
+    print(f"Training complete. Model saved to {save_path}")
 
 
 # -----------------------------
@@ -80,4 +102,9 @@ def main():
 # -----------------------------
 if __name__ == "__main__":
     warnings.filterwarnings("ignore", category=UserWarning, module="f110_gym.envs.base_classes")
-    main()
+
+    parser = ArgumentParser()
+    parser.add_argument("--model", type=str, default=None, help="Path to pre-trained model (optional)")
+    args = parser.parse_args()
+
+    main(model_path=args.model)
